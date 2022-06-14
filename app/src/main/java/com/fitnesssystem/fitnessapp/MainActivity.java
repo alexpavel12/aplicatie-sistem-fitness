22e804761f7c2578b2a1d24da097b523e49a2f93
AlexPavel12 <adpavel11@gmail.com>
2022-06-14 19:45:35 +0300

    Redesigned Training layout

--- app/src/main/java/com/fitnesssystem/fitnessapp/MainActivity.java
+++ app/src/main/java/com/fitnesssystem/fitnessapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.fitnesssystem.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Resources resources = getResources();
+
+        SharedPreferences trainingPreferences = getSharedPreferences("Training Data", Context.MODE_PRIVATE);
+
+        boolean workoutReady = trainingPreferences.getBoolean("Workout ready", false);
+
         ImageButton settingsButton = findViewById(R.id.button_settings);
         settingsButton.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+        });
+
+        Button trainingButton = findViewById(R.id.button_training);
+        trainingButton.setText(workoutReady ? resources.getString(R.string.start_workout) : resources.getString(R.string.training));
+        trainingButton.setOnClickListener(view -> {
+            startActivity(new Intent(this, TrainingActivity.class));
         });
 
         Button recoveryButton = findViewById(R.id.button_recovery);
---
+++ app/src/main/java/com/fitnesssystem/fitnessapp/TrainingActivity.java
@@ -0,0 +1,20 @@
+package com.fitnesssystem.fitnessapp;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class TrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training);
+    }
+}
